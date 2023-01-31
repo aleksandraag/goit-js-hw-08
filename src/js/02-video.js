532bfb1959a34d35fc26ac4bf886e0fa82a13675
aleksandraag <ola.golinska@vp.pl>
2023-01-31 23:31:07 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Vimeo from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+console.log(iframe);
+
+const player = new Vimeo(iframe);
+
+player.on('loaded', () => {
+  const currentTime = localStorage.getItem(KEY) || 0;
+  player.setCurrentTime(currentTime);
+});
+
+player.on(
+  'timeupdate',
+  _throttle(({ seconds }) => {
+    localStorage.setItem(KEY, seconds);
+  }, 1000)
+);
